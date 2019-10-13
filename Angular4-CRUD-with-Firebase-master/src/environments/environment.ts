bf7e2eccaca1ad2db53c3182305de85e00ee2487
ab0ragab <ahmedaboragab22@gmail.com>
2019-10-13 02:17:35 +0200

    first commit

---
+++ Angular4-CRUD-with-Firebase-master/src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    
+    apiKey: "AIzaSyC--j7pE4lPfjMQsYWfIuFpE6dTi8ntY_k",
+    authDomain: "employee-register-cbdf7.firebaseapp.com",
+    databaseURL: "https://employee-register-cbdf7.firebaseio.com",
+    projectId: "employee-register-cbdf7",
+    storageBucket: "employee-register-cbdf7.appspot.com",
+    messagingSenderId: "171929170222",
+    appId: "1:171929170222:web:f38eb98e1ec1b54b"
+  }
+};
